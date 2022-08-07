089e6123b04bd9e140694b1f8b414a9139ffc769
eryudi-m <eryudi-m@student.42sp.org.br>
2022-08-06 22:04:10 -0300

    change type to unsigned long long for base change in ft_printf

--- source/ft_print_x.c
+++ source/ft_print_x.c
@@ -6,7 +6,7 @@
 /*   By: eryudi-m <eryudi-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/06 21:10:15 by eryudi-m          #+#    #+#             */
-/*   Updated: 2022/08/06 21:24:22 by eryudi-m         ###   ########.fr       */
+/*   Updated: 2022/08/06 21:58:43 by eryudi-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,18 @@ int	ft_print_x(unsigned int number, char spec)
 	char	*hex;
 
 	bytes = 0;
-	if (!number)
+	if (number == 0)
+		return (ft_print_string("0"));
+	/*if (!number)
 	{
 		bytes = ft_print_string("(nil)");
 		return (bytes);
-	}
+	}*/
 	if (spec == 'x')
 		base = "0123456789abcdef";
 	else
 		base = "0123456789ABCDEF";
-	hex = ft_itoa_base((unsigned long) number, base);
+	hex = ft_itoa_base((unsigned long long) number, base);
 	bytes += ft_print_string(hex);
 	free(hex);
 	return (bytes);
