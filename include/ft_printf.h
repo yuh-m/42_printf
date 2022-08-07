3b831ca44f10a42b1723a7e7f0feaa252e377d20
eryudi-m <eryudi-m@student.42sp.org.br>
2022-08-06 21:42:56 -0300

    fix norm

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -5,24 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: eryudi-m <eryudi-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/04 00:25:32 by eryudi-m          #+#    #+#             */
-/*   Updated: 2022/08/06 21:32:18 by eryudi-m         ###   ########.fr       */
+/*   Created: 2022/08/06 21:38:41 by eryudi-m          #+#    #+#             */
+/*   Updated: 2022/08/06 21:42:29 by eryudi-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRINTF_H
-# define PRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-#include <unistd.h>
-#include <stdarg.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
 
-int		ft_printf(const char *format, ... );
+int		ft_printf(const char *format, ...);
 int		ft_print_char(char chr);
 int		ft_print_string(char *chr);
 int		ft_print_integer(int number);
 int		ft_print_unsigned_int(unsigned int number);
-int		ft_print_pointer(void *);
+int		ft_print_pointer(void *pointer);
 int		ft_print_x(unsigned int number, char specifier);
 
 char	*ft_itoa(int n);
@@ -30,6 +30,6 @@ int		len_unsigned(unsigned int number);
 char	*ft_itoa_base(unsigned long long n, char *base);
 size_t	ft_strlen(const char *chr);
 void	ft_putchar_fd(const char chr, int fd);
-void	ft_putnbr_unsig(unsigned int, int fd);
+void	ft_putnbr_unsig(unsigned int number, int fd);
 
 #endif
